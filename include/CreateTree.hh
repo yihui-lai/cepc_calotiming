e156414786a1ffa6be69c5cf7274b33204e56a62
Marco Toliman Lucchini <marco.toliman.lucchini@cern.ch>
2019-03-07 17:10:54 +0100

    code clean up, addition of Cherenkov photon counting, track counting, primary momentum

--- include/CreateTree.hh
+++ include/CreateTree.hh
@@ -23,43 +23,69 @@ public:
   CreateTree (TString name);
   ~CreateTree () ;
   
-  TTree*             GetTree() const { return ftree; };
-  TString            GetName() const { return fname; };
-  void               AddEnergyDeposit(int index, float deposit);
-  void               AddScintillationPhoton(int index);
-  void               AddCerenkovPhoton(int index);
+  TTree*          GetTree() const { return ftree; };
+  TString         GetName() const { return fname; };
+  void             AddEnergyDeposit(int index, float deposit);
+  void             AddScintillationPhoton(int index);
+  void             AddCerenkovPhoton(int index);
   int                Fill();
-  bool               Write(TFile *);
-  void               Clear() ;
+  bool             Write(TFile *);
+  void             Clear() ;
   
   static CreateTree* Instance() { return fInstance; } ;
   static CreateTree* fInstance;
   
   int Event;
+
+  int inputTrackerX0;
+  int inputServiceAlmm;
+  int inputTimingThick;
+  int inputE1Thick;
+  int inputE2Thick;
+  int inputE1Width;
+  int inputTimingECAL_dist;
+
   
   std::vector<float>* inputMomentum ; // Px Py Pz E
   std::vector<float>* inputInitialPosition ; // x, y, z
 
+  std::vector<float>* primaryMomT1 ; // Px Py Pz E
+  std::vector<float>* primaryPosT1 ; // x, y, z
+
+  std::vector<float>* primaryMomE1 ; // Px Py Pz E
+  std::vector<float>* primaryPosE1 ; // x, y, z
+
+  int nTracksT1;
+  int nTracksT2;
+  int nTracksE1;
+  int nTracksE2;
+  int nTracksTRK[6];
+
   //integrated energy in each longitudinal layer
   float depositedEnergyTotal;
 
-  float depositedEnergyAbsorber;
-
   float depositedEnergyTiming_f;
   float depositedEnergyTiming_r;
-
   float depositedEnergyECAL_f;
   float depositedEnergyECAL_r;
+  float depositedEnergyHCAL;
+
+  int tot_phot_cer_Timing_f;
+  int tot_phot_cer_Timing_r;
+  int tot_phot_cer_ECAL_f ;
+  int tot_phot_cer_ECAL_r ;
+  int tot_phot_cer_HCAL;
+
 
   float depositedEnergyWorld;
 
+  float Edep_Tracker_layer[6];
 
   //energy deposit in each trasnversally segmented channel
   float Edep_Timing_f_ch[18];
   float Edep_Timing_r_ch[18];
-
-  float Edep_ECAL_f_ch[25];
-  float Edep_ECAL_r_ch[25];
+  float Edep_ECAL_f_ch[100];
+  float Edep_ECAL_r_ch[100];
 
 
   /*
@@ -84,6 +110,12 @@ public:
   std::vector<float> time_prod_cher_ref;
   */
 
+
+  TH1F* h_phot_cer_lambda_Timing_f ;
+  TH1F* h_phot_cer_lambda_Timing_r;
+  TH1F* h_phot_cer_lambda_ECAL_f ;
+  TH1F* h_phot_cer_lambda_ECAL_r;
+  TH1F* h_phot_cer_lambda_HCAL;
 
   /*
 
